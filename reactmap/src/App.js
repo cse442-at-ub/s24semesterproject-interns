db884445664945c6d76e6f9e447ccccfb72160f8
zhuohany <zhuohanye@gmail.com>
2024-02-17 02:07:26 -0500

    Create the map

--- reactmap/src/App.js
+++ reactmap/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-function App() {
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '800px',
+  height: '500px',
+};
+const center = {
+  lat: 42.886448, // default latitude
+  lng: -78.878372, // default longitude
+};
+
+const App = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = 'map'>
+        <input id ='search' type="text" placeholder="Search your ideal location" name="search"></input>
+        <button id = '' type="submit" formAction=''>Submit</button>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={5}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </div>
   );
-}
+};
 
 export default App;
