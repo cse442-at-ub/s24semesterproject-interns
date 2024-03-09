15dfba61dc3e660018c2737f8f245fb10597e4e5
zhuohany <zhuohanye@gmail.com>
2024-03-09 03:01:04 -0500

    This commit is relevant to the new Trello card #56: Map page API update and CSS improvement.  The changes made was purely to Update the Google MAP API because Google decided to make a lot of the functions I am currently using decripate(I am still using some older functions due to them being free), and also improve upon CSS, and general functionality.

--- function.js
+++ function.js
@@ -1,74 +1,71 @@
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+    key: "MY_KEY",
+    v: "weekly",
+    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+            // Add other bootstrap parameters as needed, using camel case.
+});
+
 let currentmap;
-let service;
-let infowindow;
-
-function intitMap() {
-    var location = { lat: 43.0018, lng: -78.788173 };
-    var service;
-    var currentmap;
-    /*var map = new google.maps.Map(document.getElementById("map"),{
-        map:map,
-        zoom:4,
-        center:location
-    });*/
-
-    infowindow = new google.maps.InfoWindow();
-
-    currentmap = new google.maps.Map(
-        document.getElementById('map'), { center: location, zoom: 15 });
-
-    var request = {
+let rlocation
+ // initMap is now async
+ async function initMap() {
+    //Location for testing, UB North
+    const location = { lat: 43.0018, lng: -78.788173 };
+    //const location = { lat: 23, lng: -78.788173 }
+    // Request libraries when needed, not in the script tag.
+    const { Map } = await google.maps.importLibrary("maps");
+    const {Place} = await google.maps.importLibrary("places");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    
+    //Create New Map
+    const currentmap = new Map(document.getElementById("map"), {
+        center: { lat: 43.0018, lng: -78.788173 },
+        zoom: 15,
+        mapId: "TeamInternsMap",
+    });
+    //Create marker on location
+    
+    const request = {
         query: 'University At Buffalo',
         fields: ['name', 'geometry'],
     };
-
     var service = new google.maps.places.PlacesService(currentmap);
-    var getl;
+
     service.findPlaceFromQuery(request, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             getl = results[0]
-            for (var i = 0; i < results.length; i++) {
-                //createMarker(results[i]);
-                currentmap.setCenter(results[0].geometry.location);
-                const marker = new google.maps.Marker({
-                    currentmap,
-                    position: results[i].geometry.location,
-                });
+            location = results[0].geometry.location
+            marker = new AdvancedMarkerElement({
+                map: currentmap,
+                position:  results[0].geometry.location,
+            });
+            /*for (var i = 0; i < results.length; i++) {
                 google.maps.event.addListener(marker, "click", () => {
                     infowindow.setContent(results[0].name || "");
                     infowindow.open(currentmap);
                 });
-            }
+            }*/
         }
     });
+    var service = new google.maps.places.PlacesService(currentmap);
     service.nearbySearch(
-        { location: location, radius: 500, type: "store" },
+        { location: location, radius: 500, type: ["library"]},
         (results, status, pagination) => {
-            if (status !== "OK" || !results) return;
-            addPlaces(results, currentmap);
+             if (status !== "OK" || !results) return;
+             addPlaces(results, currentmap);
         },
     );
-
 }
 
-
-function addPlaces(places, map) {
+//Add place to result list
+async function addPlaces(places, map) {
     const placesList = document.getElementById("places");
 
     for (const place of places) {
         if (place.geometry && place.geometry.location) {
-            const image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25),
-            };
-
-            new google.maps.Marker({
-                map,
-                icon: image,
-                title: place.name,
+            const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+            marker = new AdvancedMarkerElement({
+                map: map,
                 position: place.geometry.location,
             });
 
@@ -83,3 +80,4 @@ function addPlaces(places, map) {
         }
     }
 }
+initMap();
---
+++ google.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+        </style>
+        <link rel="stylesheet" type="text/css" href="./style.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="welcom.php">
+                <button type="submit">Return to welcome page</button>
+            </a>  
+            <a href="local.html">
+                <button type="submit">Retake quiz</button>
+            </a>
+        </div>
+        <div id = "container">
+            <div id = "map">
+            </div>
+            <div id="sidebar">
+                <h2>Results</h2>
+                <ul id="places"></ul>
+            </div>
+        </div>
+        <script src = "./function.js">
+        </script> 
+        <div class="footer">
+            <p>Intern Limited</p>
+        </div>
+
+        
+    </body>
+</html>
