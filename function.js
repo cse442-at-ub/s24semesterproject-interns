5198851040c3758e0a1b7b9dc068fd85d4b6400d
zhuohany <zhuohanye@gmail.com>
2024-03-09 16:16:34 -0500

    Further functionality enhancement. No specific User story as this is not final. Location now uses the request query's location instead of having to type in a specific lat and long coordinate.

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "MY_KEY",
+    key: "My_key",
     v: "weekly",
     // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
             // Add other bootstrap parameters as needed, using camel case.
@@ -26,7 +26,8 @@ let rlocation
     //Create marker on location
     
     const request = {
-        query: 'University At Buffalo',
+        //query: 'University At Buffalo',
+        query: 'Buffalo, NY',
         fields: ['name', 'geometry'],
     };
     var service = new google.maps.places.PlacesService(currentmap);
@@ -34,30 +35,25 @@ let rlocation
     service.findPlaceFromQuery(request, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             getl = results[0]
-            location = results[0].geometry.location
+            //location = results[0].geometry.location
             marker = new AdvancedMarkerElement({
                 map: currentmap,
                 position:  results[0].geometry.location,
             });
-            /*for (var i = 0; i < results.length; i++) {
-                google.maps.event.addListener(marker, "click", () => {
-                    infowindow.setContent(results[0].name || "");
-                    infowindow.open(currentmap);
-                });
-            }*/
+
+            currentmap.setCenter(results[0].geometry.location);
+            var service = new google.maps.places.PlacesService(currentmap);
+            service.nearbySearch(
+                { location: results[0].geometry.location, radius: 4500, type: ["restaurant"], keyword: ["American"]},
+                (results, status, pagination) => {
+                    if (status !== "OK" || !results) return;
+                    addPlaces(results, currentmap);
+                },
+            );
         }
     });
-    var service = new google.maps.places.PlacesService(currentmap);
-    service.nearbySearch(
-        { location: location, radius: 500, type: ["library"]},
-        (results, status, pagination) => {
-             if (status !== "OK" || !results) return;
-             addPlaces(results, currentmap);
-        },
-    );
 }
 
-//Add place to result list
 async function addPlaces(places, map) {
     const placesList = document.getElementById("places");
 
