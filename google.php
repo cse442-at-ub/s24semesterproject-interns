f3b68a735e49d83628ed9077c0508823ec13f6c4
zhuohany <zhuohanye@gmail.com>
2024-03-09 20:20:51 -0500

    Card number 48, Map page narrow down to location. 1/2 complete. Awaiting Quiz page backend.

--- google.php
+++ google.php
@@ -1,3 +1,25 @@
+<?php
+     $mysqli = new mysqli("oceanus.cse.buffalo.edu:3306", "shengans", '50404824', "cse442_2024_spring_team_f_db");
+     if ($mysqli->connect_error) {
+         die("Connection failed: " . $mysqli->connect_error);
+     } else {
+     }
+
+    //$sql = "SELECT id, destined_location, user_city FROM user_data";
+
+    $sql2 = "SELECT * FROM user_data";
+    $result = mysqli_query($mysqli, $sql2); 
+    
+    $row = mysqli_fetch_array($result);
+    while($row = mysqli_fetch_assoc($result)) {
+        $id =$row["id"];
+        $state = $row["destined_location"];
+        $city = $row["user_city"];
+    }
+    $loc = $city . ", " . $state;
+    $mysqli->close();
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -14,7 +36,7 @@
             <a href="local.html">
                 <button type="submit">Retake quiz</button>
             </a>
-            <a id = "loc">Buffalo, NY</a>
+            <a id = "loc"><?php echo $loc?></a>
         </div>
         <div id = "container">
             <div id = "map">
@@ -23,15 +45,94 @@
                 <h2>Recommendations base on your quiz:</h2>
                 <ul id="places"></ul>
             </div>
-        </div>
-        <script src = "./function.js">
-        </script> 
+        </div> 
         <div class="footer">
             <h2>Intern Limited, 2024 </h2>
             <a href ="https://cloud.google.com/maps-platform/terms/"> Term of Service</a>
             <a href ="https://policies.google.com/privacy"> Privacy Policy</a>
         </div>
+        <!-- <script src = "./function.js">-->
+        <script>
+            (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+                key: "MY_KEY",
+                v: "weekly",
+                // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+                        // Add other bootstrap parameters as needed, using camel case.
+            });
 
+            let currentmap;
+            let rlocation;
+            // initMap is now async
+            async function initMap() {
+                //Location for testing, UB North
+                const location = { lat: 43.0018, lng: -78.788173 };
+                //const location = { lat: 23, lng: -78.788173 }
+                // Request libraries when needed, not in the script tag.
+                const { Map } = await google.maps.importLibrary("maps");
+                const {Place} = await google.maps.importLibrary("places");
+                const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+                
+                //Create New Map
+                const currentmap = new Map(document.getElementById("map"), {
+                    center: { lat: 43.0018, lng: -78.788173 },
+                    zoom: 15,
+                    mapId: "TeamInternsMap",
+                });
+                var php_var = "<?php echo $loc; ?>";
+                //Create marker on location;
+                const request = {
+                    //query: 'University At Buffalo',
+                    query: php_var,
+                    fields: ['name', 'geometry'],
+                };
+                var service = new google.maps.places.PlacesService(currentmap);
+
+                service.findPlaceFromQuery(request, function (results, status) {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        getl = results[0]
+                        //location = results[0].geometry.location
+                        marker = new AdvancedMarkerElement({
+                            map: currentmap,
+                            position:  results[0].geometry.location,
+                        });
+
+                        currentmap.setCenter(results[0].geometry.location);
+                        var service = new google.maps.places.PlacesService(currentmap);
+                        service.nearbySearch(
+                            { location: results[0].geometry.location, radius: 4500, type: ["restaurant"], keyword: ["American"]},
+                            (results, status, pagination) => {
+                                if (status !== "OK" || !results) return;
+                                addPlaces(results, currentmap);
+                            },
+                        );
+                    }
+                });
+            }
+
+            async function addPlaces(places, map) {
+                const placesList = document.getElementById("places");
+
+                for (const place of places) {
+                    if (place.geometry && place.geometry.location) {
+                        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+                        marker = new AdvancedMarkerElement({
+                            map: map,
+                            position: place.geometry.location,
+                        });
+
+                        const li = document.createElement("li");
+
+                        li.textContent = place.name;
+                        placesList.appendChild(li);
+                        li.addEventListener("click", () => {
+                            map.setCenter(place.geometry.location);
+                            map.setZoom(20)
+                        });
+                    }
+                }
+            }
+            initMap();
+        </script>
         
     </body>
 </html>
